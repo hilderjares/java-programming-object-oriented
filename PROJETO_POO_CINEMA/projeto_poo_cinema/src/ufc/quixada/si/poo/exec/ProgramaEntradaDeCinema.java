00ffcd365ad411dd139a1a5d4e9ed85f3eb3e9a1
Francisco <hider1195@gmail.com>
2018-09-18 22:40:15 -0300

    Projeto cinema em java

---
+++ PROJETO_POO_CINEMA/projeto_poo_cinema/src/ufc/quixada/si/poo/exec/ProgramaEntradaDeCinema.java
@@ -0,0 +1,44 @@
+package ufc.quixada.si.poo.exec;
+
+import ufc.quixada.si.poo.model.EntradaDeCinema;
+
+public class ProgramaEntradaDeCinema 
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		EntradaDeCinema primeiraVenda = new EntradaDeCinema("Homem-Aranha", "10:20", 1, 1, 30.50f);
+		EntradaDeCinema segundaVenda = new EntradaDeCinema("Homem-Aranha", "10:20", 1, 2, 30.50f);
+		EntradaDeCinema terceiraVenda = new EntradaDeCinema("Homem-Aranha", "10:20", 1, 3, 30.50f);
+		
+		float descontoPrimeiraVenda = primeiraVenda.calculaValorComDesconto(15, 10, 2005);
+		float descontoSegundaVenda = segundaVenda.calculaValorComDesconto(02, 04, 2010);
+		float descontoTerceiraVenda = terceiraVenda.calculaValorComDesconto(10, 9, 1989);
+		
+		float descontoCarteiraEstudantePrimeiraVenda = primeiraVenda.calculaValorComDesconto(15, 04, 2007, 456);
+		float descontoCarteiraEstudanteSegundaVenda = segundaVenda.calculaValorComDesconto(02, 04, 2010, 123);
+		float descontoCarteiraEstudanteTerceiraVenda = terceiraVenda.calculaValorComDesconto(10, 9, 1989, 789);
+
+		System.out.println("(ENGRESSO 1)");
+		System.out.println(primeiraVenda);
+		System.out.println(descontoPrimeiraVenda < primeiraVenda.getvalorOriginal() ? "O desconto foi de: " + descontoPrimeiraVenda : "Não houve descontos");
+		System.out.println("Valor com o desconto da carteira: " + descontoCarteiraEstudantePrimeiraVenda);
+		System.out.println(primeiraVenda.realizarVenda());
+		System.out.println(primeiraVenda.realizarVenda());
+		
+		System.out.println("\n(ENGRESSO 2)");
+		System.out.println(segundaVenda);
+		System.out.println(descontoSegundaVenda < segundaVenda.getvalorOriginal() ? "O desconto foi de: " + descontoSegundaVenda : "Não houve descontos");
+		System.out.println("Valor com o desconto da carteira: " + descontoCarteiraEstudanteSegundaVenda);
+		System.out.println(segundaVenda.realizarVenda());
+		System.out.println(segundaVenda.realizarVenda());
+		
+		System.out.println("\n(ENGRESSO 3)");
+		System.out.println(terceiraVenda);
+		System.out.println(descontoTerceiraVenda < terceiraVenda.getvalorOriginal() ? "O desconto foi de: " + descontoTerceiraVenda : "N�o houve descontos");
+		System.out.println("Valor com o desconto da carteira: " + descontoCarteiraEstudanteTerceiraVenda);
+		System.out.println(terceiraVenda.realizarVenda());
+		System.out.println(terceiraVenda.realizarVenda());
+
+	}
+}
---
+++ PROJETO_POO_CINEMA/projeto_poo_cinema/src/ufc/quixada/si/poo/model/EntradaDeCinema.java
@@ -0,0 +1,102 @@
+package ufc.quixada.si.poo.model;
+
+public class EntradaDeCinema 
+{
+	private String titulo;
+	private String horario;
+	private int sala;
+	private int poltrona;
+	private float valorOriginal;
+	private boolean disponivel = true;
+	
+	private int diaAtual = 03;
+	private int mesAtual = 8;
+	private int anoAtual = 2018;
+	
+	public EntradaDeCinema(String titulo, String horario, int sala, int poltrona, float valorOriginal) 
+	{
+		this.titulo = titulo;
+		this.horario = horario;
+		this.sala = sala;
+		this.poltrona = poltrona;
+		this.valorOriginal = valorOriginal;
+	}
+	
+	public String getTitulo() 
+	{
+		return titulo;
+	}
+	
+	public String getHorario() 
+	{
+		return horario;
+	}
+	
+	public int getSala() 
+	{
+		return sala;
+	}
+	
+	public int getPoltrona() 
+	{
+		return poltrona;
+	}
+	
+	public float getvalorOriginal() 
+	{
+		return valorOriginal;
+	}
+	
+	public float calculaValorComDesconto(int dia, int mes, int ano)
+	{	
+		if(dia < diaAtual && mes < mesAtual && ((anoAtual - ano) < 12) || ((anoAtual - ano) < 12 - 1))
+		{
+			return getvalorOriginal() - (getvalorOriginal() * 0.5f);
+		}
+		return getvalorOriginal();  
+	}
+	
+	public float calculaValorComDesconto(int dia, int mes, int ano, int carteiraEstudante)
+	{	
+		if((dia <= diaAtual && mes <= mesAtual && (((anoAtual - ano) >= 12) && ((anoAtual - ano) <= 15))) || ((anoAtual - ano) <= 14))
+		{
+			return getvalorOriginal() - (getvalorOriginal() * 0.4f);
+		}
+		else if(dia <= diaAtual && mes <= mesAtual && (((anoAtual - ano) >= 16) && ((anoAtual - ano) <= 20)) || ((anoAtual - ano) <= 19))
+		{
+			return getvalorOriginal() - (getvalorOriginal() * 0.3f);
+		}
+		else if((anoAtual - ano) > 20)
+		{
+			return getvalorOriginal() - (getvalorOriginal() * 0.2f);
+		}
+		return getvalorOriginal(); 
+	}
+	
+	public String realizarVenda()
+	{
+		if(this.disponivel)
+		{
+			this.disponivel = false;
+			return "Venda realizada com sucesso";
+		}
+		return "Venda já foi realizada";
+	}
+	
+	@Override
+	public String toString() 
+	{	
+		// TODO Auto-generated method stub
+		String informacoes_venda = "";
+		
+		informacoes_venda += "[Titulo: " + getTitulo() + "]\n";
+		informacoes_venda += "[Horario: " + getHorario() + "]\n";
+		informacoes_venda += "[Sala: " + getSala() + "]\n";
+		informacoes_venda += "[Poltrona: " + getPoltrona() + "]\n";
+		informacoes_venda += "[Volor Total: " + getvalorOriginal() + "]";
+		
+		return informacoes_venda;
+	}
+	
+	
+}
